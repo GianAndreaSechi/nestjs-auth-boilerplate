186ad9f0b7257cf9593e19c484313acb494d1c7f
GianAndreaSechi <sechi90@hotmail.it>
2023-05-14 12:47:30 +0200

    Add user response dto

---
+++ api/v1/src/modules/users/entities/dto/user.response.dto.ts
@@ -0,0 +1,22 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsString, IsNotEmpty, IsOptional, IsInt } from 'class-validator';
+
+export class UserResponseDto {
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    public username: string;
+
+    // Gets only validated if it's part of the request's body
+    //@IsOptional()
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    public email: string;
+
+    // Gets only validated if it's part of the request's body
+    @ApiPropertyOptional()
+    @IsOptional()
+    @IsString()
+    public role?: string;
+}
