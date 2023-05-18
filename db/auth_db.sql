012185a5ebec5cb2a1e0a2d2cab4acc1aa2670c5
GianAndreaSechi <sechi90@hotmail.it>
2023-05-18 15:38:14 +0200

    add new db dump

--- db/auth_db.sql
+++ db/auth_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Creato il: Mag 08, 2023 alle 17:32
+-- Creato il: Mag 18, 2023 alle 15:36
 -- Versione del server: 10.4.18-MariaDB
 -- Versione PHP: 8.0.3
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `myPocket`
+-- Database: `auth_db`
 --
 
 -- --------------------------------------------------------
@@ -40,15 +40,7 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `username`, `password`, `email`, `role`) VALUES
-(1, 'test', '1234', 'me@test.it', 'Admin'),
-(2, 'test2', '4567', 'me@test2.it', 'user'),
-(3, 'test3', '98910', 'me@test3.it', 'user'),
-(4, 'test4', 'prova', 'me@test4.it', 'user'),
-(6, 'test5', 'prova', 'me@test5.it', 'user'),
-(7, 'test6', '', 'me@test6.it', 'user'),
-(8, 'test8', '$2b$15$CjxGyUoHIpHozaGmfWXRZO30Hd9EpmVxrSZdgcARVAUZ4KFfjzIzS', 'me@test8.it', 'user'),
-(9, 'test9', '$2b$15$wxSOYDMVnIhEEdC6L9Lpp.yPWFBLViPl36HrsUPnAxXeCxIYpM8M2', 'me@test9.it', 'user'),
-(10, 'test10', '$2b$15$wdEJ3K35ML4vgKWtCYnGreEd0RwIltVdJh5VEs/Md8W21F9ChMQ5i', 'me@test10.it', 'user');
+(13, 'Admin', '$2b$15$vf8mckXKpb3crG6USoe/yuJKMr/OIJgfvZdPiwhJsafElkngRK20e', 'me@admin.it', 'user');
 
 --
 -- Indici per le tabelle scaricate
@@ -69,7 +61,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT per la tabella `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
